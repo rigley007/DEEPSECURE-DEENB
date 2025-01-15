d514527ddbeefc353d5f6565d04a3d9fa9d3de1a
Rean <125299691+n0cct1s@users.noreply.github.com>
2025-01-15 12:36:42 -0500

    Update regular_generator.py

--- deepsecure/regular_generator.py
+++ deepsecure/regular_generator.py
@@ -1,8 +1,8 @@
 
 import torch.nn as nn
 import torchvision.models as pre_models
-from resnet_block import ResnetBlock
-from pre_model_extractor import model_extractor
+from module.resnet_block import ResnetBlock
+from module.pre_model_extractor import model_extractor
 import config as cfg
 
 class regular_generator(nn.Module):
