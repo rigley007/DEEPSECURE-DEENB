a0bf0a033b0aa1509536e0b55c78324595df773a
Rean <125299691+n0cct1s@users.noreply.github.com>
2025-01-15 12:36:55 -0500

    Update catted_generator.py

--- deepsecure/catted_generator.py
+++ deepsecure/catted_generator.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 import torch
-from resnet_block import ResnetBlock
-from pre_model_extractor import model_extractor
+from module.resnet_block import ResnetBlock
+from module.pre_model_extractor import model_extractor
 import config as cfg
 
 class catted_generator(nn.Module):
