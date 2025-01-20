a778bbd937501e8aaac33079f05a0bf5ebc011c9
Yang L. <68570071+yliu0062@users.noreply.github.com>
2025-01-20 17:32:44 -0500

    Update adv_image.py

--- deepsecure/adv_image.py
+++ deepsecure/adv_image.py
@@ -17,7 +17,7 @@ def weights_init(m):
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
 
-
+# define class Adv_Gen
 class Adv_Gen:
     def __init__(self,
                  device,
