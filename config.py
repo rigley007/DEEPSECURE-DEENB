dad966a43e84eb0281e63c9af4e9c33a0344d211
Rean <125299691+n0cct1s@users.noreply.github.com>
2025-01-20 00:44:00 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,23 @@
-
-use_cuda = True
-image_nc = 3
-epochs = 80
-batch_size = 64
 BOX_MIN = 0
 BOX_MAX = 1
-pretrained_model_arch = 'resnet18'
+image_nc = 3
 num_layers_ext = 5
 ext_fixed = True
+
+pretrained_model_arch = 'resnet18'
+use_cuda = True
+epochs = 100
+batch_size = 128
 
 G_tagged = True
 tag_size = 6
 
 cat_G = True
 noise_img = True
-noise_g_path = './models/netG_epoch_80.pth'
+noise_g_path = './models/netG_epoch_100.pth'
 
-imagenet10_traindir = 'C:/Users\Rui\Pictures/transfer_imgnet_10/train'
-imagenet10_valdir = 'C:/Users\Rui\Pictures/transfer_imgnet_10/val'
+imagenet10_traindir = './Pictures/transfer_imgnet_10/train'
+imagenet10_valdir = './Pictures/transfer_imgnet_10/val'
 
 models_path = './models/'
 adv_img_path = './images/0526/adv/'
