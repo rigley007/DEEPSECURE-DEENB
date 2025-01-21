b12616e5b25de9b970ccfe6d03c4f50725fa4913
IVYEVOL <52659747+IVYEVOL@users.noreply.github.com>
2025-01-20 22:45:00 -0500

    Update regular_generator.py

--- deepsecure/regular_generator.py
+++ deepsecure/regular_generator.py
@@ -1,9 +1,8 @@
-
 import torch.nn as nn
 import torchvision.models as pre_models
 from module.resnet_block import ResnetBlock
 from module.pre_model_extractor import model_extractor
-import config as cfg
+import config as cfg 
 
 class regular_generator(nn.Module):
     def __init__(self,
