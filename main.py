44d1f36d46180e8f6819a7ef7bad264f56c0e24d
xiongsiqi0502 <62106866+xiongsiqi0502@users.noreply.github.com>
2025-01-20 23:46:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from cat_adv_image import Cat_Adv_Gen
 from regular_generator import regular_generator
 from catted_generator import catted_generator
 from pre_model_extractor import model_extractor
-#-------------------------------
+
 if __name__ == '__main__':
     # Check if CUDA is available and print the result
     print("CUDA Available: ", torch.cuda.is_available())
