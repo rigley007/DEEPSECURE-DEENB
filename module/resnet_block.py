8377aa29b519e6556a1b1ff0f43940657dc4d4cb
zzaawy <63528364+zzaawy@users.noreply.github.com>
2025-01-20 23:44:43 -0500

    Update resnet_block.py

--- module/resnet_block.py
+++ module/resnet_block.py
@@ -1,6 +1,6 @@
 # import torch.nn as nn
 
-# # Define a resnet block
+# #Define a resnet block
 # # modified from https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/models/networks.py
 # class ResnetBlock(nn.Module):
 #     def __init__(self, dim, padding_type='reflect', norm_layer=nn.BatchNorm2d, use_dropout=False, use_bias=False):
@@ -46,7 +46,7 @@ import torch.nn as nn
 # modified from https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/models/networks.py
 
 class ResnetBlock(nn.Module):
-    """Implements a residual block as used in ResNet architectures.
+    """Implements a residual block 
     
     This block maintains the input dimensions while applying convolutions with residual
     connections. It supports different padding types, normalization, and dropout options.
